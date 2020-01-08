4a158138d034cd1dde3604533bd1831621c3c998
Thomas Jouanolle <tjouanolle@localhost.localdomain>
2020-01-08 13:48:18 +0100

    Add the resolution of the equation with the secante

---
+++ resolve_with_secante.c
@@ -0,0 +1,62 @@
+/*
+** EPITECH PROJECT, 2020
+** 105torus_2019
+** File description:
+** Resolve the equation with the secante method
+*/
+
+#include "torus.h"
+
+static double compute_x2(config_t *config, double x0, double x1)
+{
+    double x2 = 0;
+
+    x2 += get_equation_result(x1) * (x1 - x0);
+    if (get_equation_result(x1) - get_equation_result(x0) == 0) {
+        write(2, "Division by zero\n", 17);
+        exit(84);
+    }
+    x2 /= get_equation_result(x1) - get_equation_result(x0);
+    x2 *= -1;
+    x2 += x1;
+    return x2;
+}
+
+static void update_values(double *x0, double *x1, double x2)
+{
+    *x0 = *x1;
+    *x1 = x2;
+}
+
+static void print_x_value(double x, int precision)
+{
+    my_printf("x = ");
+    my_put_double(x, get_nb_decimal(x, precision));
+    my_printf("\n");
+}
+
+void resolve_xith_secante(config_t *config)
+{
+    double x0 = 0;
+    double x1 = 1;
+    double x2 = 0;
+    double a;
+
+    check_first_values(config);
+    print_x_value(0.5, config->precision);
+    for (int i = 0; i < 50; i++) {
+        x2 = compute_x2(config, x0, x1);
+        print_x_value(x2, config->precision);
+        if (x2 == 0) {
+            write(2, "Division by zero\n", 17);
+            exit(84);
+        }
+        a = ABS(x2 - x1);
+        a /= ABS(x2);
+        if (a < pow(10, -config->precision))
+            exit(0);
+        update_values(&x0, &x1, x2);
+    }
+    write(2, "No solution found\n", 18);
+    exit(84);
+}
